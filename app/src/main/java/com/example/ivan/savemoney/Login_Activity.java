ba148ceb690617fd3af19725065c5d38cc4022a7
IvanZeroi <ivan.garcia.watr@gmail.com>
2018-06-12 23:33:15 -0500

    Finalizo Front-End de la app

--- app/src/main/java/com/example/ivan/savemoney/Login_Activity.java
+++ app/src/main/java/com/example/ivan/savemoney/Login_Activity.java
@@ -1,13 +1,31 @@
 package com.example.ivan.savemoney;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class Login_Activity extends AppCompatActivity {
 
+    RelativeLayout really1,really2;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            really1.setVisibility(View.VISIBLE);
+            really2.setVisibility(View.VISIBLE);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+
+        really1 = (RelativeLayout) findViewById(R.id.really1);
+        really2 = (RelativeLayout) findViewById(R.id.really2);
+
+        handler.postDelayed(runnable,3000);
     }
 }
