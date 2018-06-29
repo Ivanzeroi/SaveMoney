bae1bb27900b7e1293b558f19d4845eb31148f07
IvanZeroi <ivan.garcia.watr@gmail.com>
2018-06-29 00:25:42 -0500

    Configuracion webView

--- app/src/main/java/com/example/ivan/savemoney/HomeActivity.java
+++ app/src/main/java/com/example/ivan/savemoney/HomeActivity.java
@@ -2,16 +2,23 @@ package com.example.ivan.savemoney;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 
 public class HomeActivity extends AppCompatActivity {
 
+    private WebView graficasPie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        inicializarViews();
+        graficasPie.loadUrl("https://www.facebook.com");
+    }
 
+    public void inicializarViews(){
+        graficasPie = (WebView) findViewById(R.id.graficasMoneyPie);
     }
 }
