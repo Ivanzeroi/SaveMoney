4d7cd0db93e3ca020d3d12e01ffea9820843d41a
IvanZeroi <ivan.garcia.watr@gmail.com>
2018-07-06 01:08:50 -0500

    New ButtonFloating

--- app/src/main/java/com/example/ivan/savemoney/HomeActivity.java
+++ app/src/main/java/com/example/ivan/savemoney/HomeActivity.java
@@ -27,16 +27,16 @@ public class HomeActivity extends AppCompatActivity {
                 " formatter: '{a} <br/>{b}: {c} ({d}%)'"+
                 "  },"+
                 "legend: {"+
-                "orient: 'vertical',"+
+                "orient: 'horizontal',"+
                 " x: 'left',"+
-                "data:['直达','营销广告','搜索引擎','邮件营销','联盟广告','视频广告','百度','谷歌','必应','其他']"+
+                "data:['直达','营销广告','搜索引擎','邮件营销']"+
                 "},"+
                 "series: ["+
                 "{"+
                 "name:'访问来源',"+
                 "type:'pie',"+
                 "selectedMode: 'single',"+
-                "radius: [0, '30%'],"+
+                "radius: [0, '32%'],"+
                 "label: {"+
                 " normal: {"+
                 " position: 'inner'"+
